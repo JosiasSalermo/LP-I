1b2b09c2e4c6d24bfd4c028aa278110ab6bce56f
Josias <josiassalermojunior>
2024-03-25 05:36:55 -0300

    4.1 Tipo Boolean

---
+++ Capitulo4/src/_4_1tipoBoolean/Exemplo.java
@@ -0,0 +1,13 @@
+package _4_1tipoBoolean;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        boolean pagamento = true;
+
+        if(pagamento){
+            System.out.println("Pagamento foi efetuado");
+        }else{
+            System.out.println("Pagamento Não foi efetuado");
+        }
+    }
+}
