796114060be45d53de4349f559d26084e7877307
Josias <josiassalermojunior>
2024-05-06 06:06:50 -0300

    Downcasting

---
+++ Capitulo4/src/casting/Downcasting.java
@@ -0,0 +1,10 @@
+package casting;
+
+public class Downcasting {
+  public static void main(String[] args) {
+    double a = 10.5;
+    int b = (int) a; // downcasting }
+
+  }
+
+}
---
+++ Capitulo4/src/casting/EvitandoErros.java
@@ -0,0 +1,18 @@
+package casting;
+
+public class EvitandoErros {
+  public static void main(String[] args) {
+    double a = 3.14;
+    int b = (int) a; // downcasting
+
+    long x = 10000;
+    int i = (int) x; // downcasting
+
+    double x2 = 0.1;
+
+    float x3 = (float) x2; // downcasting
+
+    float x4 = 0.0F;
+  }
+
+}
