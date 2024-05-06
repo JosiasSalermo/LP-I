10845d526013e76d9d681e91cf4c5ce627390e90
Josias <josiassalermojunior>
2024-05-06 05:38:08 -0300

    4.1 tipo boolean

--- Capitulo4/src/Boolean.java
+++ Capitulo4/src/Boolean.java
@@ -1,11 +1,10 @@
-public class Exemplo {
-
+public class Boolean {
   public static void main(String[] args) {
+
     boolean pagamento = true;
 
     if (pagamento) {
       System.out.println("Pagamento foi efetuado");
-
     } else {
       System.out.println("Pagamento não foi efetuado");
     }
